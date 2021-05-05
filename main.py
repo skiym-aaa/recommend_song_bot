8485ea3d332593a31f9141cd89d7fc141cb9f4ea
skiym-aaa <y.sakiyama3216@gmail.com>
2021-05-05 18:07:32 +0900

    Debug port2

--- main.py
+++ main.py
@@ -53,6 +53,12 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+    arg_parser = ArgumentParser(
+        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
+    )
+    arg_parser.add_argument('-p', '--port', type=int, default=int(os.environ.get('PORT', 8000)), help='port')
+    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    arg_parser.add_argument('--host', default='0.0.0.0', help='host')
+    options = arg_parser.parse_args()
+
+    app.run(debug=options.debug, host=options.host, port=options.port)
