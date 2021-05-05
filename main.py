a34af15b56689c558ef05ee26b2bbea188f705d8
skiym-aaa <y.sakiyama3216@gmail.com>
2021-05-05 18:26:06 +0900

    Debug port4

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ import os
 
 app = Flask(__name__)
 
-import argparse
 import recommend
 
 #環境変数取得
@@ -54,12 +53,6 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', type=int, default=int(os.environ.get('PORT', 8000)), help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    arg_parser.add_argument('--host', default='0.0.0.0', help='host')
-    options = arg_parser.parse_args()
-
-    app.run(debug=options.debug, host=options.host, port=options.port)
+#    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
